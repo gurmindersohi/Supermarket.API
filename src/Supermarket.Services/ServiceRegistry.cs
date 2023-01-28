1af58b00668d654567c93c687251f753d8cce7cd
Gurminder Singh <gurmindersingh@Gurminders-MacBook-Pro.local>
2023-01-28 18:57:07 -0500

    Added Infrastructure project and register services

---
+++ src/Supermarket.Services/ServiceRegistry.cs
@@ -0,0 +1,21 @@
+﻿namespace Supermarket.Services
+{
+    using Supermarket.Abstractions.Services;
+    using Microsoft.Extensions.Configuration;
+    using Microsoft.Extensions.DependencyInjection;
+    using Supermarket.Abstractions.Repositories;
+    using Supermarket.Data.Repositories;
+
+    public static class ServiceRegistry
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddTransient(typeof(ICategoryRepository), typeof(CategoryRepository));
+            services.AddTransient(typeof(ICategoryService), typeof(CategoryService));
+            services.AddTransient(typeof(IUnitOfWork), typeof(UnitOfWork));
+
+            return services;
+        }
+    }
+}
+
